cee1f6c715fdd88d78327c69e7d0c0e3b4192c2f
Lektro9 <larskr93@gmail.com>
2021-01-14 22:36:29 +0100

    setup some example typeorm code

---
+++ src/bootstrap.ts
@@ -0,0 +1,15 @@
+import { getRepository } from 'typeorm';
+import { User } from './entity/User';
+
+export const Bootstrap = async () => {
+  const userRepo = getRepository(User);
+  const user = userRepo.create({
+    firstName: 'Lars',
+    lastName: 'Jamesons',
+    age: 23,
+  });
+  await userRepo.save(user).catch((err) => {
+    console.log('Error: ', err);
+  });
+  console.log('new user with id: ', user.id);
+};
